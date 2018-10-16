fcfc5544508dba2560512d4ba0e2ba06381821ff
Javier Garcia <javier.garcia@lyra-network.com>
2018-10-16 16:59:49 +0200

    MOBP-133 : Remove logs and provide a rich response

--- src/main/java/com/lyra/LyraClient.java
+++ src/main/java/com/lyra/LyraClient.java
@@ -15,8 +15,26 @@ import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
-@Slf4j
+/**
+ * This client component allows to interact with the Payment Platform.<p/>
+ *
+ * In order to configure this component, it is necessary to set the properties in a file called
+ * lyra-client-configuration.properties. This file must exist in the classpath.<p/>
+ *
+ * It is possible, anyway, to set an specific configuration using the {@link LyraClientConfiguration} object
+ * and its builder.<p/>
+ *
+ * In case of error, a {@link LyraClientException} exception will be thrown, containing error details and the full stack.
+ * This exception extends {@link RuntimeException}, so it is up to developer to decide how and when to catch it.
+ *
+ * @author Lyra Network
+ */
 public class LyraClient {
+
+    private static final String SDK_VERSION = "V4";
+    private static final String CONFIGURATION_FILE_NAME = "lyra-client-configuration";
+
+    protected static final Gson GSON = new Gson();
 
     private static String defaultUsername;
     private static String defaultPassword;
@@ -39,6 +57,54 @@ public class LyraClient {
         defaultRequestTimeout = defaultConfiguration.getProperty("requestTimeout");
     }
 
+    /**
+     * Calls the payment platform in order to recover all the payment information details.
+     *
+     * @param parameters Map that contains the parameters of the payment
+     * @return LyraClientResponse
+     * @throws LyraClientException exception if error processing the request
+     */
+    public static LyraClientResponse preparePayment(Map<String, Object> parameters) throws LyraClientException {
+        return preparePayment(parameters, LyraClientConfiguration.builder().build());
+    }
+
+    /**
+     * Calls the payment platform in order to recover all the payment information details.
+     *
+     * @param parameters Map that contains the parameters of the payment
+     * @param requestConfiguration Configuration object that overrides the default configuration for this request
+     * @return
+     * @throws LyraClientException exception if error processing the request
+     */
+    public static LyraClientResponse preparePayment(Map<String, Object> parameters, LyraClientConfiguration requestConfiguration) throws LyraClientException {
+        String responseMessage = null;
+        Map<String, String> configuration = getFinalConfiguration(requestConfiguration);
+
+        try {
+            HttpURLConnection connection = createConnection("CreatePayment", configuration);
+
+            sendRequestPayload(connection, GSON.toJson(parameters));
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == 200) {
+                try (BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                    responseMessage = buffer.lines().collect(Collectors.joining("\n"));
+                }
+            } else {
+
+            }
+        } catch (IOException ioe) {
+            throw new LyraClientException("Exception calling payment platform server", ioe);
+        }
+
+        return LyraClientResponse.fromResponseMessage(responseMessage);
+    }
+
+    /*
+    This method calculates the configuration to use. It takes the default one and overrides it with the configuration
+    passed as parameter
+     */
     private static Map<String, String> getFinalConfiguration(LyraClientConfiguration requestConfiguration) {
         if (requestConfiguration == null) {
             requestConfiguration = LyraClientConfiguration.builder().build();
@@ -56,9 +122,13 @@ public class LyraClient {
         return finalConfiguration;
     }
 
+    /*
+    Read the default configuration from configuration file that should exist in classpath
+     */
     private static Properties readDefaultConfiguration() {
         Properties configurationProperties = new Properties();
-        try (InputStream input = LyraClient.class.getClassLoader().getResourceAsStream("lyra-client-configuration.properties")) {
+        try (InputStream input = LyraClient.class.getClassLoader()
+                .getResourceAsStream(CONFIGURATION_FILE_NAME + ".properties")) {
             configurationProperties.load(input);
         } catch (IOException ioe) {
             throw new LyraClientException("Could not read default configuration");
@@ -67,12 +137,18 @@ public class LyraClient {
         return configurationProperties;
     }
 
+    /*
+    Generates the Url to call Rest API
+     */
     private static String generateUrl(String resource, Map<String, String> configuration) {
         String endpoint = configuration.get("endpointURL");
 
-        return endpoint /*+ "/api-payment/" */ + "/" + resource + "/";
+        return String.format("%s/api-payment/%s/Charge/%s", endpoint, SDK_VERSION, resource);
     }
 
+    /*
+    Creates the connection used to make a JSON based REST call
+     */
     private static HttpURLConnection createConnection(String resource, Map<String, String> configuration) throws IOException {
         URL urlToConnect = new URL(generateUrl(resource, configuration));
 
@@ -85,12 +161,13 @@ public class LyraClient {
             proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyServer, Integer.parseInt(proxyPort)));
         }
 
+        //Create connection
         HttpURLConnection connection = (proxy != null) ? (HttpURLConnection) urlToConnect.openConnection(proxy)
                 : (HttpURLConnection) urlToConnect.openConnection();
 
         //add request headers
         connection.setRequestMethod("POST");
-        connection.setRequestProperty("User-Agent", "Mobile Client SDK ");
+        connection.setRequestProperty("User-Agent", "Mobile Client SDK " + SDK_VERSION);
         connection.setRequestProperty("Content-type", "application/json; charset=UTF-8");
         connection.setRequestProperty("Accept", "application/json");
         connection.setRequestProperty("Authorization", "Basic " + generateAuthorizationToken(configuration));
@@ -101,6 +178,9 @@ public class LyraClient {
         return connection;
     }
 
+    /*
+    Send an http request with the provided payload
+     */
     private static void sendRequestPayload(HttpURLConnection connection, String payload) throws IOException {
         // Send post request
         connection.setDoOutput(true);
@@ -110,36 +190,11 @@ public class LyraClient {
         }
     }
 
+    /*
+    Generates the string to send in basic authorization
+     */
     private static String generateAuthorizationToken(Map<String, String> configuration) throws UnsupportedEncodingException {
         return Base64.getEncoder().encodeToString(
                 new String(configuration.get("username") + ":" + configuration.get("password")).getBytes("UTF-8"));
-    }
-
-
-    public static String preparePayment(Map<String, Object> parameters) throws LyraClientException {
-        return preparePayment(parameters, LyraClientConfiguration.builder().build());
-    }
-
-    public static String preparePayment(Map<String, Object> parameters, LyraClientConfiguration requestConfiguration) throws LyraClientException {
-        String response = null;
-        Map<String, String> configuration = getFinalConfiguration(requestConfiguration);
-
-        try {
-            HttpURLConnection connection = createConnection("createPayment", configuration);
-
-            sendRequestPayload(connection, new Gson().toJson(parameters));
-
-            int responseCode = connection.getResponseCode();
-            log.info("Response Code: " + responseCode);
-
-            try (BufferedReader buffer = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
-                response = buffer.lines().collect(Collectors.joining("\n"));
-            }
-            log.info("Response: " + response);
-        } catch (IOException ioe) {
-            throw new LyraClientException("Exception calling payment platform server", ioe);
-        }
-
-        return response;
     }
 }
---
+++ src/main/java/com/lyra/LyraClientResponse.java
@@ -0,0 +1,68 @@
+package com.lyra;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class LyraClientResponse {
+    private String status;
+    private String version;
+
+    //Field returned in case of success
+    private String formToken;
+
+    //Fields returned in case of error
+    private String errorCode;
+    private String errorDetails;
+
+    private LyraClientResponse() {
+    }
+
+    protected static LyraClientResponse fromResponseMessage(String responseMessage) {
+        LyraClientResponse clientResponse = new LyraClientResponse();
+        Map restApiResponseMessage;
+        try {
+            restApiResponseMessage = LyraClient.GSON.fromJson(responseMessage, Map.class);
+        } catch (JsonSyntaxException jse) {
+            throw new LyraClientException("Could not read CreatePayment message", jse);
+        }
+
+        if (restApiResponseMessage != null) {
+            clientResponse.status = (String) restApiResponseMessage.get("status");
+            clientResponse.version = (String) restApiResponseMessage.get("applicationVersion");
+
+            Map<String, String> answer = (Map) restApiResponseMessage.get("answer");
+            if (answer != null) {
+                if ("SUCCESS".equals(clientResponse.status)) {
+                    clientResponse.formToken = answer.get("formToken");
+                } else {
+                    clientResponse.errorCode =
+                            generateErrorData(answer, "errorCode", "detailedErrorCode");
+                }
+                clientResponse.errorDetails =
+                        generateErrorData(answer, "errorMessage", "detailedErrorMessage");
+            } else {
+                throw new LyraClientException("No answer data found in CreatePayment message");
+            }
+        } else {
+            throw new LyraClientException("Could not read CreatePayment message");
+        }
+
+        return clientResponse;
+    }
+
+    public String toJson() {
+        return LyraClient.GSON.toJson(this);
+    }
+
+    private static String generateErrorData(Map<String, String> answer, String error, String extendedErrorData) {
+        String errorMessage = answer.get(error);
+        if (answer.get(extendedErrorData) != null) {
+            errorMessage += " - " + answer.get(extendedErrorData);
+        }
+        return errorMessage;
+    }
+}
